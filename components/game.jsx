e742ec36725d2e2c1c594c0b70f31b5f55e51dd7
Chris <moorecash@gmail.com>
2022-04-11 14:22:42 -0400

    add render with modal

--- components/game.jsx
+++ components/game.jsx
@@ -12,6 +12,25 @@ class Game extends React.Component {
     }
 
     render() {
+    let modal;
+    if (this.state.board.lost() || this.state.board.won()) {
+        const text = this.state.board.won() ? "You won!" : "You lost!";
+        modal =
+            <div className='modal-screen'>
+                <div className='modal-content'>
+                    <p>{text}</p>
+                    <button onClick={this.restartGame}>Play Again</button>
+                </div>
+            </div>;
+    }
+
+    return (
+        <div>
+            {modal}
+            <Board board={this.state.board} updateGame={this.updateGame} />
+        </div>
+    );
+}
     }
 }
 
