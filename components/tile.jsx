40280e75bf54039a44c497ffa3f8429b2f7ad50f
Chris <moorecash@gmail.com>
2022-04-11 18:12:23 -0400

    add tile

---
+++ components/tile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Tile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    
+}
+
+export default Tile;
